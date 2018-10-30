e994ca930463753671be108f702fd02b5a0c99fd
Vaibhav Satam <vaibhav.satam29991@gmail.com>
2018-10-30 09:36:58 +0530

    Update html.js

--- src/html.js
+++ src/html.js
@@ -4,12 +4,6 @@ import PropTypes from 'prop-types';
 import preloads from './head/preloads';
 import metaAndStyleSheets from './head';
 
-const propTypes = {
-  body: PropTypes.string,
-  headComponents: PropTypes.array,
-  postBodyComponents: PropTypes.array
-};
-
 class HTML extends React.Component {
   render() {
     return (
@@ -32,6 +26,10 @@ class HTML extends React.Component {
   }
 }
 
-HTML.propTypes = propTypes;
+HTML.propTypes = {
+  body: PropTypes.string,
+  headComponents: PropTypes.array,
+  postBodyComponents: PropTypes.array
+};;
 
 export default HTML;
